8fc59bf22354a7d6266ee9166bcfe6f32b982aa5
Emir Luna <luis.emir.lm@gmail.com>
2020-09-16 21:24:25 -0500

    Footer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,10 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector:  'app-header',
-  template: `
-  <h1>Angular and Spring</h1>
-  <h2>Course on udemy</h2>
-  `
+  templateUrl: './header.component.html' 
 })
 export class HeaderComponent{
 
