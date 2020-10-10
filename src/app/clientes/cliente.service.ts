2e32d44f6499e7dee9b2fea212e43e960422a28a
ELJEFE21 <7315579-El_jefe2021@users.noreply.gitlab.com>
2020-10-10 09:42:18 -0500

    Was added a handler for the errors that come from the server as bad request with code 400

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -38,6 +38,11 @@ getCliente(id): Observable<Cliente>{
 create(cliente: Cliente): Observable<Cliente>{
     return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers:this.httpheaders}).pipe(
         catchError(e => {
+
+            if(e.status==400){
+              return throwError(e);
+            }
+
             console.error(e.error.mensaje)
             swal.fire('Error al crear cliente', e.error.mensaje, 'error');
             return throwError(e);
@@ -48,6 +53,11 @@ create(cliente: Cliente): Observable<Cliente>{
 update(cliente:Cliente): Observable<Cliente>{
   return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers:this.httpheaders}).pipe(
     catchError(e => {
+
+      if(e.status==400){
+        return throwError(e);
+      }
+
         console.error(e.error.mensaje)
         swal.fire('Error al actualizar cliente', e.error.mensaje, 'error');
         return throwError(e);
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -12,7 +12,7 @@ import swal from 'sweetalert2';
 export class FormComponent implements OnInit {
   public cliente: Cliente = new Cliente();
   public titulo: string = "Crear cliente";
-
+  public errores: string[];
 
   constructor(private clienteService: ClienteService,
               private router: Router,
@@ -39,7 +39,12 @@ export class FormComponent implements OnInit {
     .subscribe(cliente => {
      this.router.navigate(['/clientes'])
      swal.fire('Nuevo Cliete', `Cliente ${cliente.name} creado con exito!`, 'success')
-    }
+   },
+   err => {
+     this.errores = err.error.errors as string[];
+     console.error('Codigo de error enviado por el servidor: '+ err.status);
+     console.error(err.error.errors);
+   }
     );
     }
 
@@ -48,7 +53,13 @@ export class FormComponent implements OnInit {
       .subscribe( cliente => {
         this.router.navigate(['/clientes'])
         swal.fire('Cliente Actualizado', `Cliente ${cliente.name} actualizado con exito!`, 'success')
-      })
+      },
+      err => {
+        this.errores = err.error.errors as string[];
+        console.error('Codigo de error enviado por el servidor: '+ err.status);
+        console.error(err.error.errors);
+      }
+    );
     }
 
 }
