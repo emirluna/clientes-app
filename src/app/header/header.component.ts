07411f43af6c63dbb9766d8040b08d454a3fa81d
ELJEFE21 <7315579-El_jefe2021@users.noreply.gitlab.com>
2021-03-07 12:58:54 -0600

    Logout function, clear the token and delete the data.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,22 @@
 import {Component} from '@angular/core';
-
+import {AuthService} from '../usuarios/auth.service';
+import {Router} from '@angular/router';
+import swal from 'sweetalert2';
 @Component({
   selector:  'app-header',
   templateUrl: './header.component.html' 
 })
+
 export class HeaderComponent{
+
+
+constructor(public authService: AuthService,
+            private router: Router){}
+
+logout():void{  
+  swal.fire('Logout', `${this.authService.usuario.username}, Ha cerrado sesión exitosamente`, 'success');
+  this.authService.logout();
+  this.router.navigate(['/login']);
+}
 
 }
--- src/app/usuarios/auth.service.ts
+++ src/app/usuarios/auth.service.ts
@@ -74,6 +74,24 @@ public get token(): string{
 }
 
 
+isAuthenticated(): boolean{
+  let payload = this.obtenerToken(this.token);
+  if(payload != null && payload.user_name && payload.user_name.length>0){
+    return true;
+  }
+  return false;
+}
+
+
+
+logout():void{
+  this._token = null;
+  this._usuario = null;
+
+  sessionStorage.clear();
+  sessionStorage.removeItem('token');
+  sessionStorage.removeItem('usuario');
+}
 
 
 }
